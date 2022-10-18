8fe86cd72f1cb81a943bbb180700189040ae5d7b
Shalane Proctor <shalane088@gmail.com>
2022-10-18 18:17:05 -0500

    test

---
+++ views/customers_requests.py
@@ -0,0 +1,18 @@
+CUSTOMERS = [
+    {
+        "id": 1,
+        "name": "Jeremiah",
+    },
+    {
+        "id": 2,
+        "name": "Michael",
+    },
+    {
+        "id": 3,
+        "name": "Mitch",
+    },
+    {
+        "id": 4,
+        "name": "Brett",
+    }
+]
---
+++ views/employee_requests.py
@@ -0,0 +1,19 @@
+EMPLOYEES = [
+    {
+        "id": 1,
+        "name": "Shay",
+    },
+    {
+        "id": 2,
+        "name": "Krima",
+    }
+  ,
+    {
+        "id": 3,
+        "name": "Nishaya",
+    },
+    {
+        "id": 42,
+        "name": "Jessica",
+    }
+]
